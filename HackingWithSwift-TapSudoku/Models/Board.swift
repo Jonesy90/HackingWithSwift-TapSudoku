457db2978b4bc9554915b4057a2cf16ba7ee1342
Michael Jones <michael.jones90@me.com>
2026-04-03 10:27:04 +0100

    - Difficulty Enum:     - added to adjust the difficulty level and the amount of cells which will be empty.

--- HackingWithSwift-TapSudoku/Models/Board.swift
+++ HackingWithSwift-TapSudoku/Models/Board.swift
@@ -8,15 +8,29 @@
 import Foundation
 
 struct Board: Equatable {
+    enum Difficulty: Int, CaseIterable {
+        #if DEBUG
+        case testing = 2
+        #endif
+        case trivial = 10
+        case easy = 20
+        case medium = 24
+        case hard = 27
+        case extreme = 29
+    }
+    
     let size = 9
+    let difficulty: Difficulty
     var fullBoard = [[Int]]()
     var playerBoard = [[Int]]()
     
-    init() {
+    init(difficulty: Difficulty = .easy) {
+        self.difficulty = difficulty
         create()
+        prepareForPlay()
     }
     
-    mutating private func create() {
+    private mutating func create() {
         let numbers = Array(1...size)
         let positions = [0, 3, 6, 1, 4, 7, 2, 5, 8]
         
@@ -36,5 +50,17 @@ struct Board: Equatable {
         }
         
         playerBoard = fullBoard
+    }
+    
+    private mutating func prepareForPlay() {
+        let empties = difficulty.rawValue
+        let allCells = 0..<size * size / 2
+        
+        for cell in allCells.shuffled().prefix(upTo: empties) {
+            let row = cell / size
+            let column = cell % size
+            playerBoard[row][column] = 0
+            playerBoard[8 - row][8 - column] = 0
+        }
     }
 }
